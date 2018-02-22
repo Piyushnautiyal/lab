1f020c0dbc9f52e8919966878d9ed8b5bc466c40
piyush.nautiyal <piyush.nautiyal@nuance.com>
2018-02-22 22:48:03 +0530

    simplified

--- StringOperations/StringOperations/src/NumReverse.java
+++ StringOperations/StringOperations/src/NumReverse.java
@@ -4,24 +4,15 @@ public class NumReverse {
 
 	public static void main(String[] args) {
 		int a =123;
-      ArrayList< Integer> al=new ArrayList<Integer>();
 		int i;
+		int reverse	= 0;
 		while(a>0)
 		{
 			i=a%10;
-			System.out.println("i val "+i);
+			reverse	= reverse * 10 + i;
 			a=a/10;
-			System.out.println("a val "+a);
-			al.add(i);
 		}
-		int nunber=0;
-		for (Integer integer : al) {
-		
-			nunber=nunber*10+integer;
-			
-		}	
-		System.out.println(nunber);
-		
+		System.out.println("reverse is "+ reverse);
 	}
 	
 }
