67d37b4d3e2515e7b474655db65922f434643808
piyush.nautiyal <piyush.nautiyal@nuance.com>
2018-03-12 12:31:16 +0530

    HashMap test code added

---
+++ HashCodeOrEquals/src/HashMapTest/KeyClass.java
@@ -0,0 +1,17 @@
+package HashMapTest;
+
+public class KeyClass {
+
+	
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return 1;
+	}
+}
---
+++ HashCodeOrEquals/src/HashMapTest/MainClass.java
@@ -0,0 +1,21 @@
+package HashMapTest;
+
+import java.util.HashMap;
+
+public class MainClass {
+	public static void main(String args[]) {
+		KeyClass k1	= new KeyClass();
+		KeyClass k2	= new KeyClass();
+		KeyClass k3	= new KeyClass();
+		
+		HashMap hm	= new HashMap();
+		
+		hm.put(k1, "value 1");
+		hm.put(k2,"value 2");
+		hm.put(null,"value 3");
+		
+		System.out.println(hm.get(k1));
+		System.out.println(hm.get(k2));
+		System.out.println(hm.get(null));
+	}
+}
