7c9cb683090d333317776bc346e2ffdf35d91fb8
manas.bansal <manas.bansal@nuance.com>
2018-03-12 12:42:50 +0530

    Null Key Hashcode Example

--- HashCodeOrEquals/src/HashMapTest/MainClass.java
+++ HashCodeOrEquals/src/HashMapTest/MainClass.java
@@ -13,6 +13,9 @@ public class MainClass {
 		hm.put(k1, "value 1");
 		hm.put(k2,"value 2");
 		hm.put(null,"value 3");
+		/*
+		 * null ki hashcode always give 0 Value thats no effect on first two Objects 
+		 */
 		
 		System.out.println(hm.get(k1));
 		System.out.println(hm.get(k2));
