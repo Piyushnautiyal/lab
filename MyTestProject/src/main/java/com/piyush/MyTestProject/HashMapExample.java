793ea157618614f8c013b9f11ac3ab256b9db901
manas.bansal <manas.bansal@nuance.com>
2018-03-12 17:58:08 +0530

    HashMap Basic Get & Put where map is of String type & we are fetching using int as a key

---
+++ MyTestProject/src/main/java/com/piyush/MyTestProject/HashMapExample.java
@@ -0,0 +1,16 @@
+package com.piyush.MyTestProject;
+
+import java.util.HashMap;
+
+public class HashMapExample {
+	public static void main(String[] args) {
+		
+		HashMap<String, String> hm = new HashMap<String, String>();
+		hm.put("1", "CSE");
+		hm.put("2", "ECE");
+		hm.put("3", "IT");
+		hm.put("4", "ME");
+		System.out.println("Val from Map "+hm.get(1));
+	}
+
+}
