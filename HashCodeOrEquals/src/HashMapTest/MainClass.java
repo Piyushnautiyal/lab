26f97decdd563ec7ce733e85f149060175e236cf
piyush.nautiyal <piyush.nautiyal@nuance.com>
2018-03-12 16:32:24 +0530

    Edited with new key

--- HashCodeOrEquals/src/HashMapTest/MainClass.java
+++ HashCodeOrEquals/src/HashMapTest/MainClass.java
@@ -7,18 +7,17 @@ public class MainClass {
 		KeyClass k1	= new KeyClass();
 		KeyClass k2	= new KeyClass();
 		KeyClass k3	= new KeyClass();
+		KeyClass k4	= new KeyClass();
 		
 		HashMap hm	= new HashMap();
 		
 		hm.put(k1, "value 1");
 		hm.put(k2,"value 2");
 		hm.put(null,"value 3");
-		/*
-		 * null ki hashcode always give 0 Value thats no effect on first two Objects 
-		 */
-		
+	
 		System.out.println(hm.get(k1));
 		System.out.println(hm.get(k2));
 		System.out.println(hm.get(null));
+		System.out.println(hm.get(k4));
 	}
 }
