8a512479d76d6141e7edf3b56a1c1e6aa984195c
manasbansal295 <manashadoop295@gmail.com>
2018-04-17 23:54:01 +0530

    Array

---
+++ ArrayDataStructure/src/main/java/ArrayRotation.java
@@ -0,0 +1,35 @@
+
+public class ArrayRotation {
+	void printArray(int arr[], int size) 
+	{
+		int i;
+		for (i = 0; i < size; i++)
+			System.out.print(arr[i] + " ");
+	}
+	public void leftRotate(int arr[],int d,int size)
+	{
+		for(int i=0;i<d;i++)
+		{
+			leftRotatebyOne(arr, size);
+		}
+	}
+	public void leftRotatebyOne(int arr[],int n)
+	{
+		int i ,temp;
+		temp = arr[0];
+		for(i=0;i<n-1;i++)
+			arr[i] = arr[i+1];
+		arr[i] = temp;
+	}
+	public static void main(String[] args) 
+	{
+		ArrayRotation rotate = new ArrayRotation();
+		int arr[] = {1, 2, 3, 4, 5, 6, 7};
+		rotate.leftRotate(arr, 4, 7);
+		rotate.printArray(arr, 7);
+	}
+
+
+
+}
+
