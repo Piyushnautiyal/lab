50ff8ae8bc52525773a708ba2411d34c6348e2e6
piyush.nautiyal <piyush.nautiyal@nuance.com>
2018-05-11 16:47:43 +0530

    Some extreme cases added

--- FailSafeAndFailFast/src/main/java/ConcurrentTest.java
+++ FailSafeAndFailFast/src/main/java/ConcurrentTest.java
@@ -1,4 +1,5 @@
 import java.util.Iterator;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ConcurrentTest {
@@ -12,17 +13,33 @@ public static void main(String[] args) {
 		concurrentHashMap.put("3", "Sonia");
 		concurrentHashMap.put("4", "Suraj");
 		
-		Iterator<String> itr = concurrentHashMap.keySet().iterator();
+		Set set = concurrentHashMap.keySet();
 		
-		concurrentHashMap.put("0", "TP");
+		Iterator<String> itr	= set.iterator();
 		
+		concurrentHashMap.put(".1", "TP");
+		
+		System.out.println("Set1 is ["+set+"] size of map "+concurrentHashMap.size());
+		
+		set.remove("1");
+		
+		System.out.println("Set1 is ["+set+"] size of map "+concurrentHashMap.size());
+		
+		concurrentHashMap.put("6", "Rajat");
+		concurrentHashMap.put("7", "Rohit");
+		concurrentHashMap.put("8", "Naveen");
+		concurrentHashMap.put("0", "Sanjeev");
+		concurrentHashMap.put("9", "Naveen");
+
 		while(itr.hasNext())
 		{
+			
 			String  key = itr.next();
 			concurrentHashMap.put("5","HUM");
 			System.out.println("Key "+key+" Value "+concurrentHashMap.get(key)+" Size of map "+concurrentHashMap.size() );
 			concurrentHashMap.remove("3");
 		}
+		
 	}
 	catch(Exception ee)
 	{
