42198e5c4513c77bb3930f16d29b51a882c9ae24
manasbansal295 <manashadoop295@gmail.com>
2018-05-16 22:32:33 +0530

    Basic functioning of interface

---
+++ MyTestProject/src/main/java/com/basic/Inteface/functioning/B.java
@@ -0,0 +1,6 @@
+package com.basic.Inteface.functioning;
+
+public interface B {
+	void rust();
+
+}
---
+++ MyTestProject/src/main/java/com/basic/Inteface/functioning/TestingInterface.java
@@ -0,0 +1,24 @@
+package com.basic.Inteface.functioning;
+
+public class TestingInterface implements A,B {
+
+	public static void main(String[] args) {
+		TestingInterface obj = new TestingInterface();
+/*
+ * This example is to show only that two interface having same method and a class implementing both interfaces
+ * will not give any compile time error
+ */
+		obj.rust();
+		//	obj.rust1();
+	}
+
+	public void rust() {
+		System.out.println("Called Rust");		
+	}
+
+	/*public void rust1() {
+		System.out.println("Called Rust1");		
+
+	}*/
+
+}
