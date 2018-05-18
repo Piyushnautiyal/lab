e64c6753c119af7772dcf47c202410fd36923c51
manasbansal295 <manashadoop295@gmail.com>
2018-05-18 11:50:55 +0530

    copy on write list printed which will shows the difference

--- FailSafeAndFailFast/src/main/java/CopyOnWriteDemo.java
+++ FailSafeAndFailFast/src/main/java/CopyOnWriteDemo.java
@@ -11,12 +11,17 @@ public class CopyOnWriteDemo {
 		list.add("Element3");
 		list.add("Element4");
 
+		/*
+		 * when we create an iterator for the CopyOnWriteArrayList, 
+		 * we get an immutable snapshot of the data in the list at the time iterator() was called.
+		 */
 		Iterator<String> listIterator	= list.iterator();
 		
 		list.add("Element1");
 		
 		while(listIterator.hasNext()) {
 			System.out.println("Value from list is ["+listIterator.next()+"]");
+			System.out.println("list is "+list);
 			list.add("Element5");
 			list.remove("Element3");
 		}
