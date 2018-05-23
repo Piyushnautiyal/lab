e704bef71be618377996c4e5c7e22a80d7eb7e30
piyush.nautiyal <piyush.nautiyal@nuance.com>
2018-05-23 18:10:34 +0530

    Red black tree explanation.

---
+++ HashCodeOrEquals/src/com/test/piyush/TreeMap/PackageInfo.java
@@ -0,0 +1,13 @@
+package com.test.piyush.TreeMap;
+
+public class PackageInfo {
+
+	/**
+	 * 
+	 * Red Black tree has the following properties :
+	 * 1. As the name of the algorithm suggests ,color of every node in the tree is either red or black.
+	 * 2. Root node must be Black in color.
+	 * 3. Red node can not have a red color neighbor node.
+	 * 4. All paths from root node to the null should consist the same number of black nodes 
+	 */
+}
