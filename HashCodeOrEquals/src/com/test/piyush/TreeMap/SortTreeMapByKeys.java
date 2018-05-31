fa5a9b426063a5022f91d08865be7816e491229c
manasbansal295 <manashadoop295@gmail.com>
2018-05-31 22:02:50 +0530

    Minor changes for readability

--- HashCodeOrEquals/src/com/test/piyush/TreeMap/SortTreeMapByKeys.java
+++ HashCodeOrEquals/src/com/test/piyush/TreeMap/SortTreeMapByKeys.java
@@ -6,12 +6,12 @@ import java.util.TreeMap;
 public class SortTreeMapByKeys {
 	public static void main(String[] args) {
 		TreeMap<Employee,String> records	= new TreeMap<Employee,String>(new MyComparator());
-		
+		//TreeMap<Employee,String> records	= new TreeMap<Employee,String>();
 		records.put(new Employee("Ramesh",5),"DEV");
 		records.put(new Employee("Aman",1),"DEV");
 		records.put(new Employee("Zayan",2),"DEV");
 		records.put(new Employee("Ritesh",4),"DEV");
-		
+
 		System.out.println(records);
 	}
 }
@@ -33,35 +33,53 @@ class MyComparator implements Comparator<Employee>{
 }
 
 
-//This should implement comparable
-class Employee implements Comparable<Employee>{
+//This should implement comparable if comparator is not given in constructor
+/*class Employee implements Comparable<Employee>{
 
 	public int id;
 	public String name;
-	
+
 	public Employee(String name,int id) {
 		this.name	= name;
 		this.id		= id;
 	}
-	
+
 	@Override
 	public int hashCode() {
 		return super.hashCode();
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
 		return super.equals(obj);
 	}
 
-	
+
 	//THIS WILL SORT BY NAME AS A DEFAULT NATURAL ODERING
 	@Override
 	public int compareTo(Employee o) {
 		// TODO Auto-generated method stub
 		return this.name.compareTo(o.name);
+		//return this.id - o.id;
 	}
-	
+
+	@Override
+	public String toString() {
+		return ""+this.name + " " + this.id;
+	}
+}*/
+
+/**************************************************************************/
+
+// Employee Class in case we are providing Comparator class in constructor
+class Employee{
+	public int id;
+	public String name;
+
+	public Employee(String name,int id) {
+		this.name	= name;
+		this.id		= id;
+	}
 	@Override
 	public String toString() {
 		return ""+this.name + " " + this.id;
