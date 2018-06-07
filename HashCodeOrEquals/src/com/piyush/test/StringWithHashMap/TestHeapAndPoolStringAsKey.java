50393bd432e16234b628a36e90b5f99e6ce09bab
piyush.nautiyal <piyush.nautiyal@nuance.com>
2018-06-07 19:44:52 +0530

    FutureTask and HashMap trick question added

---
+++ HashCodeOrEquals/src/com/piyush/test/StringWithHashMap/TestHeapAndPoolStringAsKey.java
@@ -0,0 +1,21 @@
+package com.piyush.test.StringWithHashMap;
+
+import java.util.HashMap;
+
+public class TestHeapAndPoolStringAsKey {
+
+    public static void main(String[] args) {
+        String a    = new String("RaM");
+        String b    = "Ram";
+        
+        System.out.println("a==b "+(a==b));
+        System.out.println("a.equals(b) "+a.equals(b));
+        System.out.println("a.hashCode()==b.hashCode() "+(a.hashCode()==b.hashCode()));
+        HashMap<String, String> hm  = new HashMap<String,String>();
+        
+        hm.put(a,"value1");
+        hm.put(b,"value2");
+        
+        System.out.println(hm.size());
+    }
+}
---
+++ Threads/src/main/java/com/Callable/Future/FutureTaskDemo.java
@@ -0,0 +1,45 @@
+package com.Callable.Future;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+
+public class FutureTaskDemo {
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        /*
+         * FutureTask implemets RunnableFuture implements Runnable and Callable.
+         */
+        
+        FutureTask<String> futureTask   = new FutureTask<>(new SomeCallableTask());
+        
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+        Future<?> future                = executorService.submit(futureTask,"Piyush");
+        
+        
+        Thread.sleep(3000);
+        
+        if(futureTask.isDone()) {
+            System.out.println("From FutureTask "+futureTask.get());
+            System.out.println("From Future "+future.get());
+        }else {
+            System.out.println("Future task is not done");
+        }
+    }
+}
+
+class SomeCallableTask implements Callable<String>{
+
+    @Override
+    public String call() throws Exception {
+        try {
+            Thread.sleep(2000);
+        }catch(Exception e) {
+            
+        }
+        return "Callabel completed";
+    }
+}
